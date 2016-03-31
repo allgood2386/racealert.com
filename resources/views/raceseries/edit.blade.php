27534eae64c7b3f3f568a9142394c2ef9c690946
Rich Allen <rallen@taoti.com>
2016-03-31 09:42:03 -0400

    laravel5

--- resources/views/raceseries/edit.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('app')
-
-@section('content')
-    <h1>Edit: {!! $raceSeries->event_name !!}</h1>
-    {!! Form::model($raceSeries, ['method' => 'PATCH', 'action' => ['RaceSeriesController@update', $raceSeries->id]]) !!}
-    @include('raceseries._form', ['submitButtonText' => 'Edit Series'])
-    {!! Form::close() !!}
-@stop
--- resources/views/welcome.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('app')
-
-@section('content')
-    <h1>Racealert</h1>
-@endsection
