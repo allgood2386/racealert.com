7bab3e08fb9b256b6568d26d7e63160c115c942b
Rich Allen <rich.allen@bixal.com>
2017-07-07 07:43:05 -0400

    starting over again SHOCKING

--- resources/views/raceseries/create.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('app')
-@section('content')
-    <h1>Create a new Series</h1>
-    {!! Form::open(['url' => 'raceseries']) !!}
-    @include('raceseries._form', ['submitButtonText' => 'Create Series'])
-    {!! Form::close() !!}
-@endsection
--- resources/views/raceweekends/create.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('app')
-@section('content')
-    <h1>Create a new RaceWeekend</h1>
-    {!! Form::open(['url' => 'raceweekends']) !!}
-    @include('raceweekends._form', ['submitButtonText' => 'Create RaceWeekend'])
-    {!! Form::close() !!}
-@endsection
